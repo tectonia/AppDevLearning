99c880834d17c1cb7cb77d94165df0a8f17630c1
Martyna Marcinkowska <61530975+tectonia@users.noreply.github.com>
2023-10-25 09:11:19 +0100

    Delete exercisefiles/dotnet directory

--- exercisefiles/dotnet/MinimalAPI.Tests/Usings.cs
+++
@@ -1,3 +0,0 @@
-global using Xunit;
-global using Microsoft.AspNetCore.Mvc.Testing;
-global using Microsoft.Extensions.Hosting;
--- exercisefiles/dotnet/MinimalAPI/Usings.cs
+++
@@ -1,5 +0,0 @@
-global using System.Diagnostics;
-global using System.Text.RegularExpressions;
-global using System.Text.Json;
-global using System.Text.Json.Serialization;
-global using System.IO.Compression;
